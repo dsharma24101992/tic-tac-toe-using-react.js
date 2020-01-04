7a15c5480fe1a937068b796a747c7370461a0ba8
Deepak Sharma <dsharma24101992@gmail.com>
2020-01-04 19:57:07 +0530

    tic-tac-toe

---
+++ build/precache-manifest.874955c92eaba9305572bf825fc14b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861bc3ba10653dfc0fea9540f6640f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6222ac0939ba4502e8c",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "aeea1a39e473f68631f4",
+    "url": "/static/js/2.a73bb008.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a73bb008.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6222ac0939ba4502e8c",
+    "url": "/static/js/main.d5c1c7ba.chunk.js"
+  },
+  {
+    "revision": "f3f5290baee2c8ad6d1a",
+    "url": "/static/js/runtime-main.fec439dd.js"
+  }
+]);
